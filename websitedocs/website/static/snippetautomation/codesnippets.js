cbb6aad335167aa3650dc624986df93d1d0c1d83
Tiffany Zheng <qz3@williams.edu>
2018-08-10 15:12:35 -0500

    testing regex

--- websitedocs/website/static/snippetautomation/codesnippets.js
+++ websitedocs/website/static/snippetautomation/codesnippets.js
@@ -86,8 +86,8 @@ Promise
               .getAttribute('data-portions'));
             for (j = 0; j < portions.length; j++) {
               currrentString = portions[j][0];
-              console.log(currentString);
-              console.log(j);
+              //console.log(currentString);
+              //console.log(j);
               if (currrentString.startsWith("/") && currrentString.endsWith("/")) {
                 startIndex = dataFromSource.search(eval(currrentString));
               } else {
